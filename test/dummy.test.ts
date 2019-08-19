33482607957385ead5102c5989727314471244ec
kevinpollet <pollet.kevin@gmail.com>
2019-08-19 18:33:22 +0400

    test: configure and add Jest (#25)

---
+++ test/dummy.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file.
+ */
+
+describe("dummy", () => {
+  it("should be true", () => expect(true).toBeTruthy());
+});
