af868615c8828865fae013e97250910b074ae918
kevinpollet <pollet.kevin@gmail.com>
2019-08-20 09:47:49 +0400

    refactor(cli): remove unnecessary tar option

--- bin/docku.js
+++ bin/docku.js
@@ -24,9 +24,8 @@ program
     path => (!isAbsolute(path) ? resolve(process.cwd(), path) : process.cwd()),
     process.cwd()
   )
-  .option("--tar", "output the build context tarball to stdout")
   .action(options => {
-    buildImage(options)
+    buildImage(options.cwd)
       .then(stream => stream.pipe(process.stdout))
       .catch(err => console.log(err));
   });
