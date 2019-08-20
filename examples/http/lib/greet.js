b9592fe548f4d9a615cb06867d24385a3f27e2a2
kevinpollet <pollet.kevin@gmail.com>
2019-08-20 11:49:51 +0400

    feat(examples): add HTTP example (#16)
    Close #16

---
+++ examples/http/lib/greet.js
@@ -0,0 +1,12 @@
+/**
+ * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file.
+ */
+
+const greet = name => `Hello, ${name || "World"}!`;
+
+module.exports = {
+  greet,
+};
