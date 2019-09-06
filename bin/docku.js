554eee745b1d46cd5b99a9f001642a083c6eecd5
kevinpollet <pollet.kevin@gmail.com>
2019-09-06 18:54:12 +0400

    refactor(cli): add use strict instruction to bin source

--- bin/docku.js
+++ bin/docku.js
@@ -5,6 +5,7 @@
  * Use of this source code is governed by an MIT-style license that can be
  * found in the LICENSE.md file.
  */
+"use strict";
 
 const program = require("commander");
 const { isAbsolute, resolve } = require("path");
