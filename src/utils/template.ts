9889103e5faceb1a5765d946cbe1e15abc802c80
kevinpollet <pollet.kevin@gmail.com>
2019-09-17 09:53:17 +0400

    refactor(core): rename params of template methods

--- src/utils/template.ts
+++ src/utils/template.ts
@@ -6,11 +6,11 @@
  */
 
 const ifTruthy = <T>(value: Readonly<T> | undefined) => (
-  fn: ((ctx: Readonly<T>) => string) | string
+  fn: ((arg: Readonly<T>) => string) | string
 ): string => (value ? (typeof fn === "string" ? fn : fn(value)) : "");
 
 const ifNotEmpty = <T>(value: Readonly<Array<T>> | undefined) => (
-  fn: ((ctx: Readonly<Array<T>>) => string) | string
+  fn: ((arg: Readonly<Array<T>>) => string) | string
 ): string =>
   value && value.length > 0 ? (typeof fn === "string" ? fn : fn(value)) : "";
 
