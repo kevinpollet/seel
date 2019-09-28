8c7bdd146dfac109d42c3334debb1b076da39622
kevinpollet <pollet.kevin@gmail.com>
2019-09-28 10:52:06 +0400

    refactor(cli): use cumulative flags instead of comma separated values

--- src/cli.ts
+++ src/cli.ts
@@ -22,27 +22,27 @@ program
   )
   .option(
     "--entrypoint <path>",
-    "Define the app entrypoint path relative to the current working directory."
+    "Define the app entrypoint. The given path must be relative to the current working directory."
   )
   .option(
-    "--labels <labels>",
-    "Define the container image labels as a comma-separated list of key-value pairs.",
-    (labels: string) => labels.split(",").map(label => label.split("="))
-  )
-  .option("--name <name>", "Define the container image name.")
-  .option(
-    "--exposedPorts <ports>",
-    "Define the ports that the app exposes at runtime as a comma-separated list of values.",
-    (ports: string) => ports.split(",").map(port => port.trim())
+    "--exposedPort <port>",
+    "Define the port that the app exposes at runtime.",
+    (exposedPort: string, prev: string[]) => (prev || []).concat(exposedPort)
   )
   .option(
     "--extraFiles <glob>",
     "Define the extra files to include in the container image with a glob pattern."
   )
   .option(
-    "--tags <tags>",
-    "Define the container image tags as a comma-separated list of values.",
-    (tags: string) => tags.split(",").map(tag => tag.trim())
+    "--label <label>",
+    "Define the container image label.",
+    (label: string, prev: string[][]) => (prev || []).concat([label.split("=")])
+  )
+  .option("--name <name>", "Define the container image name.")
+  .option(
+    "--tag <tag>",
+    "Define the container image tag.",
+    (tag: string, prev: string[]) => (prev || []).concat(tag)
   )
   .action(({ cwd, ...rest }) => {
     const errorHandler = (err: Error): void => {
@@ -50,7 +50,14 @@ program
       process.exit(1);
     };
 
-    buildImage(cwd, rest)
+    buildImage(cwd, {
+      entrypoint: rest.entrypoint,
+      exposedPorts: rest.exposedPort,
+      extraFiles: rest.extraFiles,
+      labels: rest.label,
+      name: rest.name,
+      tags: rest.tags,
+    })
       .then(stream => stream.on("error", errorHandler).pipe(process.stdout))
       .catch(errorHandler);
   });
